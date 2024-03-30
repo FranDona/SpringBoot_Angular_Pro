a18d876e94fdd2c0999cd28adef97840d688073c
FranDona <frandonavillar@gmail.com>
2024-03-30 17:24:58 +0100

    [+] Ejemplo profesor angular

---
+++ elex_angular/src/app/app.module.ts
@@ -0,0 +1,30 @@
+//ELEX: SpringBoot3.2 + Angular17.3 -> Paso5: Controlador componente GENERAL
+
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { FormulariosTiposComponent } from './formularios-tipos/formularios-tipos.component';
+
+
+// Hay que añadir estos componentes de Librerias Angular
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    FormulariosTiposComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
--- elex_angular17/src/app/models/tipos-expediente.model.ts
+++
@@ -1,8 +0,0 @@
-
-// Ambos camps son opcionales
-export class TiposExpediente {
-    constructor(
-        public id?: number, 
-        public materia?: string
-    ) {}
-}
