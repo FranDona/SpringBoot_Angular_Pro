06b1df89053df30eb2b003f268115e3a008aeeb6
FranDona <frandonavillar@gmail.com>
2024-04-03 19:20:28 +0200

    [#] Correciones modal para insertar, actualizar y papelera

--- elex_frontend/src/app/tipos-expediente/formularios-tipos/formularios-tipos.component.ts
+++ elex_frontend/src/app/tipos-expediente/formularios-tipos/formularios-tipos.component.ts
@@ -19,6 +19,7 @@ export class FormulariosTiposComponent implements OnInit {
   mensaje: string = "";
   materia: string = "---";
   tipoParaActualizar: Tipos | null = null;
+  materiaActualizar: string = ''; // Propiedad para la materia a actualizar en el modal de actualización
 
   // Inyectar "private snackBar: MatSnackBar para Angular Material
   constructor(private servicio: TiposService, private snackBar: MatSnackBar) {}
@@ -55,7 +56,7 @@ export class FormulariosTiposComponent implements OnInit {
         this.snackBar.open('Materia duplicada o usada anteriormente', 'Cerrar', {
           duration: 3000,
           horizontalPosition: 'center',
-          verticalPosition: 'top'
+          verticalPosition: 'bottom'
         });
       }
     );
@@ -63,19 +64,19 @@ export class FormulariosTiposComponent implements OnInit {
 
 
   actualizarTipoFormulario(): void {
-    if (this.tipoParaActualizar && this.materia) {
-      this.servicio.actualizarTipo(this.tipoParaActualizar.id, this.materia).subscribe(resultado => {
+    if (this.tipoParaActualizar && this.materiaActualizar) {
+      this.servicio.actualizarTipo(this.tipoParaActualizar.id, this.materiaActualizar).subscribe(resultado => {
         this.mensaje = "Tipo actualizado";
         this.cargarTipos();
         this.tipoParaActualizar = null;
-        this.materia = '---';
+        this.materiaActualizar = ''; // Limpiar la variable después de la actualización
       });
     }
   }
 
   prepararActualizacion(tipo: Tipos): void {
     this.tipoParaActualizar = tipo;
-    this.materia = tipo.materia;
+    this.materiaActualizar = tipo.materia; // Cargar el nombre del campo que se quiere actualizar
   }
 
   cancelarActualizacion(): void {
@@ -85,10 +86,19 @@ export class FormulariosTiposComponent implements OnInit {
 
   borrarTipo(id: number): void {
     if (confirm("¿Estás seguro de querer borrar definitivamente este tipo?")) {
-      this.servicio.borrarTipo(id).subscribe(() => {
-        this.mensaje = "Tipo borrado definitivamente";
-        this.cargarTiposBorrados(); // Actualiza la lista de tipos borrados
-      });
+        this.servicio.borrarTipo(id).subscribe(() => {
+            this.mensaje = "Tipo borrado definitivamente";
+            // Actualiza la lista de tipos después de borrar definitivamente
+            this.cargarTiposBorrados(); 
+            // Vuelve a cargar los tipos después de borrar definitivamente
+            this.cargarTipos();
+            // Muestra una notificación de eliminación definitiva
+            this.snackBar.open('Tipo eliminado definitivamente', 'Cerrar', {
+                duration: 3000,
+                horizontalPosition: 'center',
+                verticalPosition: 'bottom'
+            });
+        });
     }
   }
   
@@ -99,7 +109,7 @@ export class FormulariosTiposComponent implements OnInit {
         this.snackBar.open('Tipo borrado lógicamente', 'Cerrar', {
           duration: 3000,
           horizontalPosition: 'center',
-          verticalPosition: 'top'
+          verticalPosition: 'bottom'
         });
         // Eliminar el tipo del arreglo tipos después de realizar el borrado lógico
         this.tipos = this.tipos.filter(tipo => tipo.id !== id);
@@ -112,15 +122,15 @@ export class FormulariosTiposComponent implements OnInit {
   recuperarTipo(id: number): void {
     // Recuperar un tipo borrado
     this.servicio.recuperarTipo(id).subscribe(() => {
-      this.snackBar.open('Tipo recuperado correctamente', 'Cerrar', {
-        duration: 3000,
-        horizontalPosition: 'center',
-        verticalPosition: 'top'
-      });
-      // Vuelve a cargar la lista de tipos borrados después de la recuperación
-      this.cargarTiposBorrados();
+        this.snackBar.open('Tipo recuperado correctamente', 'Cerrar', {
+            duration: 3000,
+            horizontalPosition: 'center',
+            verticalPosition: 'bottom'
+        });
+        // Vuelve a cargar los tipos después de la recuperación
+        this.cargarTipos();
     });
-  }
+}
 
 }
 
